ca112ff6ae9113e2aa2ea36d3b680b328bb33fe1
jwilliamsmith <perfiction@gmail.com>
2016-12-20 07:40:33 -0500

    add update

--- playground/mongodb-update.js
+++ playground/mongodb-update.js
@@ -8,5 +8,19 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
 	}
 	console.log('Connected to db.');
 
+	db.collection('Users').findOneAndUpdate({
+		_id: new ObjectID('58591ac9a840d00c1e8a99e7')
+	}, {
+		$set: {
+			name: "Alan"
+		},
+		$inc: {
+			age: 1
+		}
+	}, {
+		returnOriginal: false
+	})
+	.then(res => {console.log(res)});
+
 	//db.close();
 });
