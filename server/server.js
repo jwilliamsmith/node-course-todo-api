2039f40a001ba3f403a78f4e54894929a9710262
jwilliamsmith <perfiction@gmail.com>
2017-01-06 11:41:25 -0500

    add patch todos:id

--- server/server.js
+++ server/server.js
@@ -6,6 +6,7 @@ const { ObjectID } = require('mongodb');
 const { mongoose } = require('./db/mongoose');
 const { Todo } = require('./models/todo');
 const { User } = require('./models/user');
+const _ = require('lodash');
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -64,6 +65,31 @@ app.delete('/todos/:id', (req, res) => {
 			res.status(400).send();
 		});
 });
+
+app.patch('/todos/:id', (req, res) => {
+	let id = req.params.id,
+		body = _.pick(req.body, ['text', 'completed']);
+		if (!ObjectID.isValid(id)) {
+			res.status(404).send();
+		}
+		if (_.isBoolean(body.completed) && body.completed) {
+			body.completedAt = new Date().getTime();
+		} else {
+			body.completed = false;
+			body.completedAt = null;
+		}
+		Todo.findByIdAndUpdate(id, {$set: body}, {new: true})
+			.then(todo =>{
+				if (!todo) {
+					return res.status(404).send();
+				}
+				res.send({todo});
+			}).catch(err => {
+				res.status(400).send();
+			})
+
+
+})
 
 app.listen(port, () =>{
 	console.log("Started on port "+port);
